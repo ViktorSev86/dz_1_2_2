d3919c57319760c3e3b440f7d33d6d8d3cf8af4d
VictorSev86 <butioxa_86@mail.ru>
2022-03-08 18:03:53 +0300

    Correct ...11

--- src/main/kotlin/dz_1_2_2.kt
+++ src/main/kotlin/dz_1_2_2.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val likes = 61
-    val res = if (likes % 10 != 1) "людям" else "человеку"
+    val likes = 211
+    val res = if (likes % 100 == 11) "человекам" else if (likes % 10 != 1) "людям" else "человеку"
     println("Понравилось " + likes + " " + res)
 }
