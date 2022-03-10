3143584f52344e8a331bb667c4536851e5d5aff8
VictorSev86 <butioxa_86@mail.ru>
2022-03-10 22:07:00 +0300

    Correct padege

--- src/main/kotlin/dz_1_2_2.kt
+++ src/main/kotlin/dz_1_2_2.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val likes = 211
-    val res = if (likes % 100 == 11) "человекам" else if (likes % 10 != 1) "людям" else "человеку"
-    println("Понравилось " + likes + " " + res)
+    val likes = 201
+    val res = if (likes % 100 == 11 || likes % 10 != 1) "людям" else "человеку"
+    println("Понравилось $likes $res")
 }
